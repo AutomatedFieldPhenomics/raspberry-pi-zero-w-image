8b558d07d36383ea20c34dc235d84737441d31af
Wouter Deconinck <wdconinc@gmail.com>
2020-05-26 10:11:50 -0500

    timedatectl needs active systemd, so back to links...

--- scripts.d/02-timezone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Setup script error handling see https://disconnected.systems/blog/another-bash-strict-mode for details
-set -uo pipefail
-trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
-IFS=$'\n\t'
-
-timedatectl set-timezone America/Winnipeg
