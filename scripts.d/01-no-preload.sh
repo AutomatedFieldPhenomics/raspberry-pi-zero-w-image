e841d9d9e5229a7953347c3e20c0bb3498b63ccd
Wouter Deconinck <wdconinc@gmail.com>
2020-05-26 09:32:22 -0500

    Move ld.so.preload handling into create-image

--- scripts.d/01-no-preload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Setup script error handling see https://disconnected.systems/blog/another-bash-strict-mode for details
-set -uo pipefail
-trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
-IFS=$'\n\t'
-
-# Preload causes errors
-rm -f /etc/ld.so.preload
