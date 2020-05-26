86604ae2b28576b4e95f8b14b916bbc49a33ec85
Wouter Deconinck <wdconinc@gmail.com>
2020-05-26 11:05:04 -0500

    So, can't copy timezone links over existing ones, have to create them

---
+++ scripts.d/02-timezone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Setup script error handling see https://disconnected.systems/blog/another-bash-strict-mode for details
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+# Create link
+echo "America/Winnipeg" > /etc/timezone
+ln -sf /usr/share/zoneinfo/America/Winnipeg /etc/localtime
