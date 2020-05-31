c1181ea4cc96c02e536fb1aac0d093c26f5f1df1
Wouter Deconinck <wdconinc@gmail.com>
2020-05-31 13:05:40 -0500

    Set WLAN regulatory domain to CA

---
+++ scripts.d/03-wlan-domain.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Setup script error handling see https://disconnected.systems/blog/another-bash-strict-mode for details
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+# Set WLAN regulatory domain
+echo "country=CA" >> /etc/wpa_supplicant/wpa_supplicant.conf
