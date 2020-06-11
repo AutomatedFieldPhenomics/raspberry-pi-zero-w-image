872cddd55b4d131c7608c6d32b27256669202b4d
Wouter Deconinck <wdconinc@gmail.com>
2020-06-11 10:29:21 -0500

    Clean apt cache at end of build

---
+++ scripts.d/98-apt-get-clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Setup script error handling see https://disconnected.systems/blog/another-bash-strict-mode for details
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+# Clean downloaded apt files
+apt-get clean
