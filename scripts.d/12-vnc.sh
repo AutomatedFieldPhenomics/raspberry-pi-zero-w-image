95b5197265fd8420a4628f098359c8cef18d63c2
ReactiveAlkali <nedeleca@myumanitoba.ca>
2020-06-29 20:12:02 -0500

    Changed to start a VNC server at boot

--- scripts.d/12-vnc.sh
+++ scripts.d/12-vnc.sh
@@ -11,3 +11,7 @@ apt install -y realvnc-vnc-server realvnc-vnc-viewer
 # Enable VNC
 systemctl enable vncserver-x11-serviced.service 
 systemctl start vncserver-x11-serviced.service
+
+# Start VNC server at boot
+sed -i '/^# Print/i vncserver' /etc/rc.local
+
