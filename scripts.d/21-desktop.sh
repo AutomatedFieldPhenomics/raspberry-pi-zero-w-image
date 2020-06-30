d548205b0c8a42fc593e76c85b79b54feba9dad0
ReactiveAlkali <nedeleca@myumanitoba.ca>
2020-06-30 13:41:04 -0500

    Install Desktop

---
+++ scripts.d/21-desktop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Setup script error handling see https://disconnected.systems/blog/another-bash-strict-mode for details
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+# Install display server 
+apt install -y xerver-xorg
+
+# Install desktop environment
+sudo apt install -y xfce4 xfce4-terminal
+
+# Install display manager
+apt install -y lightdm
