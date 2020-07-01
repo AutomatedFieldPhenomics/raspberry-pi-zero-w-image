44b67b25ff4e9731f968d29f265815f4935df765
ReactiveAlkali <nedeleca@myumanitoba.ca>
2020-06-30 19:41:28 -0500

    Changed to tightVNC

--- scripts.d/12-vnc.sh
+++ scripts.d/12-vnc.sh
@@ -6,11 +6,10 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 IFS=$'\n\t'
 
 # Install VNC
-apt install -y realvnc-vnc-server realvnc-vnc-viewer
+apt install -y tightvncserver
 
 # Enable VNC
 systemctl enable vncserver-x11-serviced.service 
-systemctl start vncserver-x11-serviced.service
 
 # Start VNC server at boot
 sed -i '/^# Print/i vncserver' /etc/rc.local
