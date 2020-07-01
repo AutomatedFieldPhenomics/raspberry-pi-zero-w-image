c203850439a59c63be092fed9e9818fd11aac6e0
ReactiveAlkali <nedeleca@myumanitoba.ca>
2020-07-01 11:10:20 -0500

    Fixed errors

--- scripts.d/11-desktop.sh
+++ scripts.d/11-desktop.sh
@@ -5,11 +5,8 @@ set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 IFS=$'\n\t'
 
-# Install display server 
-apt install -y xserver-xorg
-
 # Install desktop environment
 sudo apt install -y xfce4 xfce4-terminal
 
-# Install display manager
-apt install -y lightdm
+# Install GUI network manager
+apt install -y network-manager
