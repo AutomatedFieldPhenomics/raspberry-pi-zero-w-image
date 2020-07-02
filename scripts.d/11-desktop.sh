136f0adb0e3881180f43a08b98872d51d65de790
ReactiveAlkali <nedeleca@myumanitoba.ca>
2020-07-01 19:03:02 -0500

    Changed to LXDE

--- scripts.d/11-desktop.sh
+++ scripts.d/11-desktop.sh
@@ -6,7 +6,7 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 IFS=$'\n\t'
 
 # Install desktop environment
-sudo apt install -y lxqt
+sudo apt install -y lxde
 
 # Install tools
 apt install -y network-manager nm-tray
