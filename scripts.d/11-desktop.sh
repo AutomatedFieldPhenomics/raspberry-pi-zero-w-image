6a4013b552beac8957853d1e195b4272a6b001f2
ReactiveAlkali <nedeleca@myumanitoba.ca>
2020-07-01 22:21:52 -0500

    Fixed error

--- scripts.d/11-desktop.sh
+++ scripts.d/11-desktop.sh
@@ -6,7 +6,7 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 IFS=$'\n\t'
 
 # Install desktop environment
-sudo apt install -y xfce4 xfce4-terminal
+sudo apt install -y xfce4
 
 # Install tools
 apt install -y network-manager nm-tray
