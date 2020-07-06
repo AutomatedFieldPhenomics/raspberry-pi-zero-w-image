f8d16081de651d58d64762c88f51840a3e00bf11
ReactiveAlkali <nedeleca@myumanitoba.ca>
2020-07-06 15:40:44 -0500

    RealVNC

--- scripts.d/18-vnc.sh
+++ scripts.d/18-vnc.sh
@@ -6,27 +6,27 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 IFS=$'\n\t'
 
 # Install VNC
-apt install -y tightvncserver
+apt install -y realvnc-vnc-server
 
 # Create startup file
-cat > /etc/systemd/system/tightvncserver.service << EOF
-[Unit]
-Description=TightVNC remote desktop server
-After=sshd.service
+#cat > /etc/systemd/system/tightvncserver.service << EOF
+#[Unit]
+#Description=TightVNC remote desktop server
+#After=sshd.service
 
-[Service]
-Type=dbus
-ExecStart=/usr/bin/tightvncserver :1
-User=pi
-Type=forking
+#[Service]
+#Type=dbus
+#ExecStart=/usr/bin/tightvncserver :1
+#User=pi
+#Type=forking
 
-[Install]
-WantedBy=multi-user.target
-EOF
+#[Install]
+#WantedBy=multi-user.target
+#EOF
 
 # Change ownership of and make executable the startup file
-chown root:root /etc/systemd/system/tightvncserver.service
-chmod 755 /etc/systemd/system/tightvncserver.service
+#chown root:root /etc/systemd/system/tightvncserver.service
+#chmod 755 /etc/systemd/system/tightvncserver.service
 
 # Enable startup at boot
-systemctl enable tightvncserver.service
+#systemctl enable tightvncserver.service
