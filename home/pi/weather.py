4c6ff989dca0a3f2bb1aa0d2a04147bda7a7aba3
Aaron Nedelec <aaronnedelec@pop-os.localdomain>
2020-07-14 13:15:10 -0500

    Pull current weather only

--- home/pi/weather.py
+++ home/pi/weather.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python3
 
+# This script pulls the current weather data from the OpenWeather api
+
 import json
 import requests
 import datetime
 import os
 
-now = datetime.datetime.now()
+# API Variables
+cityName    = 'Winnipeg'
+countryCode = 'ca'
+apiKey      = 'f8685ff5fdb2881d8b10100e840926e8'
+baseUrl     = 'http://api.openweathermap.org/data/2.5/weather?'
 
+# Gets the directory to store weather data or creates one if it does not already exist
+now = datetime.datetime.now()
 dir = "images/%s" % now.strftime("%Y-%m-%d")
 if not os.path.exists(dir):
     os.makedirs(dir)
 
-# Enter your OpenWeatherMap API key here
-api_key = "f8685ff5fdb2881d8b10100e840926e8"
+# Gets weather data from OpenWeather
+completeUrl = baseUrl + "q=" + cityName + ',' + countryCode + '&appid=' + apiKey 
+response = requests.get(completeUrl)
 
-# base_url variable to store url
-base_url = "http://api.openweathermap.org/data/2.5/forecast?"
-
-# Give city name
-city_name = "Winnipeg, CA"
-
-# complete_url variable to store complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-
-# get method of requests module return response object
-response = requests.get(complete_url)
-
-# json method of response object convert json format data into python format data
+# Convert to JSON and store
 r = response.json()
-
-# store json to file
 with open('%s/%s.json' % (dir, now.strftime("%H:%M:%S")), 'w') as file:
     json.dump(r, file)
