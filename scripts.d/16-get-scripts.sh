918b56d444fb4a4e7d888c29e24d57421c836a66
Aaron Nedelec <aaronnedelec@pop-os.localdomain>
2020-07-15 18:50:20 -0500

    Correct Error

--- scripts.d/16-get-scripts.sh
+++ scripts.d/16-get-scripts.sh
@@ -6,7 +6,7 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 IFS=$'\n\t'
 
 # Install git
-apt-get install git
+apt-get install -y git
 
 # Get python scripts from github
 git clone https://github.com/AutomatedFieldPhenomics/pi-data-scripts /home/pi/bin
