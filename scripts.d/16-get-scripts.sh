2071273794a99437c6722641ddf715fd0a06b62c
Aaron Nedelec <aaronnedelec@pop-os.localdomain>
2020-07-16 14:38:17 -0500

    Automatically update scripts daily

--- scripts.d/16-get-scripts.sh
+++ scripts.d/16-get-scripts.sh
@@ -10,3 +10,28 @@ apt-get install -y git
 
 # Get python scripts from github
 git clone https://github.com/AutomatedFieldPhenomics/pi-data-scripts /home/pi/bin
+
+# Update scripts daily
+cat > /etc/systemd/system/scriptUpdate.service << EOF
+[Unit]
+Description=Get updated scripts from GitHub
+
+[Service]
+Type=simple
+ExecStart=/home/pi/updateScripts.sh
+EOF
+
+cat > /etc/systemd/system/scriptUpdate.timer << EOF
+[Unit]
+Description=Updates scripts daily
+
+[Timer]
+OnCalendar=daily
+
+[Install]
+WantedBy=timers.target
+EOF
+
+# Enable update timer
+systemctl daemon-reload
+systemctl enable scriptUpdate.timer
