6d4bbcce01efaf034c7f12c248c01b1b1b353c2a
Aaron Nedelec <aaronnedelec@pop-os.localdomain>
2020-07-15 20:47:39 -0500

    Enable i2c connections

--- scripts.d/19-i2c.sh
+++ scripts.d/19-i2c.sh
@@ -6,7 +6,9 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 IFS=$'\n\t'
 
 # Install neccessary packages
-apt-get install -y python-smbus
+apt-get install -y python-smbus libi2c-tools
 
 # Enable i2c interface
 sed -i '/^#.*i2c/s/^#//' /boot/config.txt
+echo "i2c-dev" >> /etc/modules
+echo "i2c-bcm2708" >> /etc/modules
